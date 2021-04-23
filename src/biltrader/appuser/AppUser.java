2eb6c1cefc2ee248e90162abcd9519852c409693
O-Hamdash <76268213+O-Hamdash@users.noreply.github.com>
2021-04-23 23:43:01 +0300

    An initial implementation of the User classes

---
+++ src/biltrader/appuser/AppUser.java
@@ -0,0 +1,104 @@
+package com.example.demo.appuser;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+/**
+ * AppUser
+ */
+public class AppUser implements UserDetails {
+
+    @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private AppUserRole appUserRole;
+    private Boolean locked;
+    private Boolean enabled;
+
+    private Boolean loggedIn;
+    private int[] listingsId;
+    private int listingCounter;
+    // private Review[] reviews; to be implemented after the Review class
+    private int[] responseTimes;
+
+    public AppUser(String username, String email, String password, AppUserRole appUserRole, Boolean locked,
+            Boolean enabled) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void createListing(int listingId) {
+        listingsId[listingCounter] = listingId;
+        listingCounter++;
+    }
+
+    public void removeListing(int listingId) {
+    }
+
+    // public void addReview(Review review){ to be implemented after the Review
+    // class
+    // }
+
+    // public void removeReview(int id) to be implemented after the Review class
+    // }
+
+}
---
+++ src/biltrader/appuser/AppUserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.appuser;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email);
+}
