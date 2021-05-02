6965bb9f820161c17962a84e4345127e68efb201
eldeeb-mohamed <eldeebm2002@gmail.com>
2021-05-02 18:58:45 +0300

    Add regex matching for validating emails

--- src/main/java/com/biltrader/api/registration/EmailValidator.java
+++ src/main/java/com/biltrader/api/registration/EmailValidator.java
@@ -1,6 +1,7 @@
 package com.biltrader.api.registration;
 
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
@@ -9,8 +10,8 @@ public class EmailValidator implements Predicate<String> {
 
     @Override
     public boolean test(String s) {
-        // TODO: Regex to validate email
-        return true;
+        // Any email containing "@" (but not starting with it) and ending with "bilkent.edu.tr"
+        return Pattern.matches("^.+@.*bilkent\\.edu\\.tr$", s);
     }
 
 }
