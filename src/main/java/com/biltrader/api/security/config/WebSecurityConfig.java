060e0c9971ef3126a8dded432a551e58f42a3d30
eldeeb-mohamed <eldeebm2002@gmail.com>
2021-05-04 03:09:32 +0300

    Partially remove limit on requests in WebSecurityConfig

--- src/main/java/com/biltrader/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/biltrader/api/security/config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/api/v*/registration/**").permitAll().anyRequest()
+        http.csrf().disable().authorizeRequests().antMatchers("/api/v*/**").permitAll().anyRequest()
                 .authenticated().and().formLogin();
     }
 
